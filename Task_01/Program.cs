96195cbeb98e96a3cd5fba990659ce591f06c7e5
Artyom Smirnov <expertvideo.as@gmail.com>
2022-06-23 21:57:42 +0300

    Решили первую задачу

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+// 10 мин
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+Console.Write("Введите целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите квадрат этого целого числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+ if(a == b * b)
+ {
+    Console.WriteLine("Ok!!!");
+ }
+ else 
+ {
+ Console.WriteLine("False!!!");
+ }
