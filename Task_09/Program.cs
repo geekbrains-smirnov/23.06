6950fc22dc07a98c1aaeebe0620ee5a2102e10bf
Aryom Smirnov <expertvideo.as@gmail.com>
2022-07-06 09:52:17 +0300

    update task_09

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -1,13 +1,27 @@
 ﻿int random = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {random}");
 
-int firstDigit = random / 10;
-int secondDigit = random % 10; // получим вторую цифру
-int maxDigit = firstDigit;
+// int firstDigit = random / 10;
+// int secondDigit = random % 10; // получим вторую цифру
+// int maxDigit = firstDigit;
 
-if (secondDigit > maxDigit) maxDigit = secondDigit;
-else maxDigit = 0;
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
 
-if(maxDigit > 0) Console.WriteLine($"Наибольшая цифа числа {random} => {maxDigit}");
-else Console.WriteLine($"Цифры числа {random} равны");
+// if (secondDigit > maxDigit) maxDigit = secondDigit;
+// else if(secondDigit == maxDigit) maxDigit = 0;
 
+// if(maxDigit > 0) 
+// Console.WriteLine($"Наибольшая цифа числа {random} => {maxDigit}");
+// else Console.WriteLine($"Цифры числа {random} равны");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if(firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+
+}
+int maxDigit = MaxDigit(random);
+string result = maxDigit > 0 ? maxDigit.ToString() : "числа равны";
+Console.WriteLine($"Наибольшая цифа числа {random} => {result}");
