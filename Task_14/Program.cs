3d78a2d568c6a1029dbf7d0a281185d9d4d8dd42
Aryom Smirnov <expertvideo.as@gmail.com>
2022-07-06 13:36:00 +0300

    add task 14

---
+++ Task_14/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 7;
+int c = 23;
+if((a%b)+(a%c)==0)
+{
+    Console.WriteLine($"Число {a} кратно {b} и {c} ");
+}
+else 
+{
+    Console.WriteLine($"Число {a} не кратно {b} и {c} ");
+}
