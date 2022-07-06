3955411593c97891b2da52247fc1e74397469d95
Aryom Smirnov <expertvideo.as@gmail.com>
2022-07-06 10:41:31 +0300

    add task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿int random = new Random().Next(100,1000);
+Console.WriteLine($"Случайное число из отрезка от 100 до 999 => {random}");
+
+int num1 = random/100;
+int num2 = random%10;
+
+Console.WriteLine($"Первая цифра числа {random} => {num1}, вторая цифра => {num2}");
