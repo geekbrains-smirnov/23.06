f22dfca46bd718debee578e9c38539d600d9d18b
Aryom Smirnov <expertvideo.as@gmail.com>
2022-07-06 11:53:19 +0300

    add task 11

---
+++ Task_11/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+double num = 0;
+
+if(Convert.ToInt32(a%b)==0)
+{
+  Console.WriteLine("Первое число кратное второму числу");
+}
+if(Convert.ToInt32(a%b)!=0)
+{
+    num=Convert.ToInt32(a%b);
+    Console.WriteLine($"Первое число не кратное второму числу и остаток => {num}");
+}
+
+
