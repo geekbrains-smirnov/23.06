1df5ec4b3b315425b47434429ce92ced235af225
Aryom Smirnov <expertvideo.as@gmail.com>
2022-07-27 11:04:15 +0300

    update task_22

---
+++ Task_22/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите проверочное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int square(int n)
+{
+     int count = 1;
+     int result = 1;
+    while (count <= n)
+    {
+        result = count * count;
+        count++;
+        Console.WriteLine(result);
+        
+    }
+    return(result);
+    
+}
+
+Console.WriteLine(square(n));
