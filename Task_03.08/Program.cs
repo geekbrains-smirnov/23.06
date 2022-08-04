1f342ffe3a2e97f86e98b2ade4c4cf647ca1956f
Aryom Smirnov <expertvideo.as@gmail.com>
2022-08-04 15:48:40 +0300

    update Task_03.08

--- Task_03.08/Program.cs
+++ Task_03.08/Program.cs
@@ -8,6 +8,8 @@
 // }
 
 
+// -----------------------------------------------------------------
+
 // Работа с текстом
 // Дан текст. В тексте нужно все пробелы заменить черточками,
 // маленькие буквы "к" заменить большими "К",
@@ -35,6 +37,7 @@
 // string newText = repalce(text, ' ', '|');
 // Console.WriteLine(newText);
 
+// -------------------------------------------------
 
 // int[] arr = {1,5,4,3,2,6,7,1,1};
 
@@ -77,6 +80,128 @@
 // SelectionSort(arr);
 
 // PrintArray(arr);
+
+// ---------------------------------------------------------------------------------
+
+// Напишите программу которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+// 7 -> 28 
+// 4 -> 10
+// 8 -> 36
+
+
+// Console.Write("Введите проверочное число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// int summa(int a)
+// {
+//     int sum = 0;
+    
+//     for (int i = 1; i <= a; i++ )
+//     {
+         
+//          sum = sum + i;  
+//     }
+//      return sum;
+
+    
+// }
+
+
+// Console.WriteLine(summa(a));
+
+//------------------------------------------------
+
+
+// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+// Console.Write("Введите проверочное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int amount(int num)
+// {
+//     int count = 0;
+//     if( num==0) count =1;
+    
+//     while (num != 0)
+//         {
+//             num = num/10;
+//             count++; 
+            
+//         }
+    
+//     return count;
+// }
+
+
+// Console.WriteLine(amount(num));
+ 
+// ------------------------------------------------------------------
+
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for(int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+
+// }
+// Console.Write("введите целое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] factor(int n)
+// { 
+// int[] array = new int[n];
+// int f =1;
+// for (int i = 1; i <= n; i++)
+//     {
+//         f = f*i;
+//         array[i-1]=f;
+//     }
+//     return array;
+// }    
+// PrintArray(factor(n));
+
+// --------------------------------------------------------------------
+
+// напишите программу, которая выводит массив из N элементов, заполненный нулями и единицами в случайном порядке
+
+
+// void PrintArray(int[] array)
+//     {
+//         int count = array.Length;
+
+//         for(int i = 0; i < count; i++)
+//         {
+//             Console.Write($"{array[i]} ");
+//         }
+//         Console.WriteLine();
+
+//     }
+// Console.Write("Введите рамер массива: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] RandomArr(int N)
+// {
+//     int[] array = new int[N];
+//     for (int i =1; i<N; i++)
+//     {
+//         array[i] = new Random().Next(0,2);
+//     }
+
+//     return array;
+// }
+
+// PrintArray(RandomArr(N));
+
+// -----------------------------------------------------------------------------
 
 
 
